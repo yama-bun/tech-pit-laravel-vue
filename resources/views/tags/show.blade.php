2e9c20d8385d21eb50ab4324880429b906f50421
yama-bun <baseball8296@gmail.com>
2021-02-23 16:46:52 +0900

    タグリンク機能追加

---
+++ resources/views/tags/show.blade.php
@@ -0,0 +1,22 @@
+@extends('app')
+
+@section('title')
+    $tag->hastag
+@endsection
+
+@section('content')
+    @include('nav')
+    <div class="container">
+        <div class="card mt-3">
+            <div class="card-body">
+                <h2 class="h4 card-title m-0">{{ $tag->hashtag }}</h2>
+                <div class="card-text text-right">
+                    {{ $tag->articles->count() }}件
+                </div>
+            </div>
+        </div>
+        @foreach ($tag->articles as $article)
+            @include('articles.card')
+        @endforeach
+    </div>
+@endsection
