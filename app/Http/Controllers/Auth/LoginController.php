7bc21350e2ab5c92a069ec85c412a8da140d5c8f
yama-bun <baseball8296@gmail.com>
2021-02-26 20:49:42 +0900

    test

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -56,5 +56,10 @@ class LoginController extends Controller
             $this->guard()->login($user, true);
             return $this->sendLoginResponse($request);
         }
+        return redirect()->route('register.{provider}', [
+            'provider' => $provider,
+            'email' => $providerUser->getEmail(),
+            'token' => $providerUser->token,
+        ]);
     }
 }
